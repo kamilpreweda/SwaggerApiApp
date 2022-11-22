3694eaff1bee1d3d86764ec637281ad67d65f0aa
Kamil Preweda <preweda.kamil@o2.pl>
2022-11-22 18:16:14 +0100

    Changed header info on Swagger

--- SwaggerApi/Program.cs
+++ SwaggerApi/Program.cs
@@ -1,18 +1,36 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Our User API (this is our title)",
+        Description = "This is the description about our API.",
+        TermsOfService = new Uri("https://www.iamtimcorey.com"),
+        Contact = new OpenApiContact
+        {
+            Name = "Tim Corey (Contact Info)",
+            Url = new Uri("https://www.iamtimcorey.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Cool License (License Name)",
+            Url = new Uri("https://www.iamtimcorey.com")
+        }
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(opts =>
-    {
-        opts.SerializeAsV2 = true;
-    });
+    app.UseSwagger();
     app.UseSwaggerUI(opts =>
     {
         opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
